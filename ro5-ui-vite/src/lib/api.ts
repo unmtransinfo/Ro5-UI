bdb2da768fd06ac57760495a666d1ecca1bbd001
Bat-Ochir Artur <amgalanenerel@gmail.com>
2025-09-26 10:48:38 -0600

    redo branch

--- ro5-ui-vite/src/lib/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  timeout: 15000,
-  
-});
-
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 
-
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 15000,
+  
 });
+
